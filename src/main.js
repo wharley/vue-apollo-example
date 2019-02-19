965f0db383b4534c98559f0e2bdb6c04fe847fde
wharleyornelasjs <wharleyornelasjs@gmail.com>
2019-02-19 00:07:14 -0300

    Added vue-apollo.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+
+Vue.use(VueApollo)
+
+const httpLink = new HttpLink({
+  uri: `https://api.graph.cool/simple/v1/cjsb2n2so22qo01541pex7kuu`
+})
+
 Vue.config.productionTip = false
 
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
 new Vue({
+  apolloProvider,
   render: h => h(App),
 }).$mount('#app')
